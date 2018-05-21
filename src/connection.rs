44120e7af80da1102866a4df5cff1a0dd381d93b
Takeru Ohta <phjgt308@gmail.com>
2018-05-21 22:38:36 +0900

    Use safe `set_nodelay` method

--- src/connection.rs
+++ src/connection.rs
@@ -34,7 +34,7 @@ impl Connection {
         dispatcher: Dispatcher,
         options: &ServerOptions,
     ) -> Result<Self> {
-        let _ = unsafe { stream.with_inner(|s| s.set_nodelay(true)) };
+        let _ = stream.set_nodelay(true);
         let base_url = format!(
             "http://{}/",
             track!(stream.local_addr().map_err(Error::from))?

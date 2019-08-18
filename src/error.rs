414badec404f558ef9405e086e6e801c1db08b11
Takeru Ohta <phjgt308@gmail.com>
2019-08-18 20:38:26 +0900

    Derive `TrackableError`

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,8 @@ use trackable::error::{Failure, TrackableError};
 use url;
 
 /// This crate specific `Error` type.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, TrackableError)]
 pub struct Error(TrackableError<ErrorKind>);
-derive_traits_for_trackable_error_newtype!(Error, ErrorKind);
 impl From<Failure> for Error {
     fn from(f: Failure) -> Self {
         ErrorKind::Other.takes_over(f).into()
